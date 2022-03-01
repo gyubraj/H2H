09a6a26ba535a50e4c98a34edbedbda142a0f2c6
Yubraj <gyubraj104@gmail.com>
2022-03-01 18:11:28 +0545

    Some initial changes

--- property/views.py
+++ property/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class AddPropertyView(View):
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class DeletePropertyView(View):
+
+    def get(self, request, pk):
+        pass
+
--- search/views.py
+++ search/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+class SearchView(View):
+
+    def get(self,request):
+        pass
+
+    def post(self, request):
+
+        location = request.POST['location']
+        start_date = request.POST['start_date']
+        end_date = request.POST['end_date']
+        type = request.POST['type']
+
+        
