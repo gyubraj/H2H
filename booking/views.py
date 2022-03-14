30787e0e495230e4c6b3af270a5d38caa93bc9c8
Yubraj <gyubraj104@gmail.com>
2022-03-14 17:13:20 +0545

    Unfinish Addorder

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from django.views import View
+
+from property.models import Property
 # Create your views here.
+
+
+class AddOrder(View):
+
+    def get(self, request, slug):
+
+        property = get_object_or_404(Property, slug= slug, available = True)
+
+        from_date = request.POST['from_date']
+        to_date = request.POST['to_date']
+        no_of_people = request.POST['no_of_people']
+        no_of_rooms = request.POST['no_of_rooms']
+
+        
+
+
+
