842430584075e142c202a1dfd789883340659185
Yubraj <gyubraj104@gmail.com>
2022-03-17 16:44:19 +0545

    Change in urls

--- booking/urls.py
+++ booking/urls.py
@@ -5,8 +5,8 @@ from booking.views import ReceivedBookingView, BookedBookingView, AddBookingView
 urlpatterns = [
     path('received-booking',ReceivedBookingView.as_view(), name="received-booking"),
     path('my-booking',BookedBookingView.as_view(),name="booked-booking"),
-    path('add-booking',AddBookingView.as_view(), name= "add-booking"),
+    path('add-booking/<str:slug>',AddBookingView.as_view(), name= "add-booking"),
     path('edit-booking/<int:id>',EditBookingView.as_view(), name= "edit-booking"),
-    path('delete-booking', DeleteBookingView.as_view(), name= "delete-booking")
+    path('delete-booking/<int:id>', DeleteBookingView.as_view(), name= "delete-booking")
 
 ]
