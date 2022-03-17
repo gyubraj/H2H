e2c8a5dc4a2b2bd8349719efed62111a78507309
Yubraj <gyubraj104@gmail.com>
2022-03-17 19:03:40 +0545

    Urls changed

--- property/urls.py
+++ property/urls.py
@@ -2,11 +2,13 @@
 
 from django.urls import path
 
-from property.views import AddPropertyView, MyProperty, PropertyDetailView, EditProperty
+from property.views import AddPropertyView, MyProperty, PropertyDetailView, EditProperty, DeletePropertyView
 
 urlpatterns = [ 
     path('add-property',AddPropertyView.as_view(),name='add-property'),
     path('edit-property/<str:slug>',EditProperty.as_view(),name="edit-property"),
     path('my-property',MyProperty.as_view(),name='my-property'),
     path('property-detail/<str:slug>',PropertyDetailView.as_view(),name="property-detail"),
+    path('delete-property/<str:slug>',DeletePropertyView.as_view(),name="delete-property"),
+    
 ]
--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 """"""
 
 from django.urls import path
-from user.views import RegisterView, LoginView, ResetPasswordRequestView, ResetPasswordView, ActivateAccount
+from user.views import RegisterView, LoginView, ResetPasswordRequestView, ResetPasswordView, ActivateAccount, LogoutUser
 
 urlpatterns = [
     path('login/',LoginView.as_view(),name='login'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('request-reset-password/', ResetPasswordRequestView.as_view(),name='request-reset-password'),
     path('reset-password/<str:uid>/<str:token>/',ResetPasswordView.as_view(),name="reset-password"),
     path('activate-account/<str:uid>/<str:token>/',ActivateAccount.as_view(),name='activate-account'),
+    path('logout',LogoutUser.as_view(), name="logout")
 ]
