4d2e05c7c4e0d974e979371704c8e1f53ad92681
Yubraj <gyubraj104@gmail.com>
2022-03-21 12:51:45 +0545

    Added Login url to point login user

--- H2H/settings.py
+++ H2H/settings.py
@@ -149,6 +149,8 @@ MEDIA_URL='media/'
 MEDIA_ROOT = BASE_DIR/ 'media'
 
 
+LOGIN_URL = 'login'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
